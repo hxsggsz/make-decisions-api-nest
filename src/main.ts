7ee8e0eb8d1016b852b9dc50dabaab64c28c515f
hxsggsz <vt.hugo.2021@gmail.com>
2023-11-22 14:36:07 -0300

    fix cors error

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(UserModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   const port = process.env.PORT || 3000;
 
   await app.listen(port, '0.0.0.0');
